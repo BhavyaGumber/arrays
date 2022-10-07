6d3187041a2a5b7ff215c0c01f39c9c527653bea
BhavyaGumber <79220494+BhavyaGumber@users.noreply.github.com>
2022-10-07 19:54:53 +0530

    Create linear_search.cpp

---
+++ linear_search.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find element in sorted array
+    // arr: input array
+    // N: size of array
+    // K: element to be searche
+    int searchInSorted(int arr[], int N, int K) 
+    { 
+    
+       // Your code here
+       for(int i=0;i<N;i++){
+           if(arr[i]==K){
+               return 1;
+           }
+         
+       
+    }
+    return -1;
+    }
+    
+};
